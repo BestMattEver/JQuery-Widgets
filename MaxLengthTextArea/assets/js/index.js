1bb795087b0473b1f28f95fb1bb37a7891425ef1
BestMatEver <mattkun@gmail.com>
2016-02-15 22:50:25 -0500

    did the max length texxt area

---
+++ MaxLengthTextArea/assets/js/index.js
@@ -0,0 +1,12 @@
+var area = $("#textArea");
+var howMuchLeft =$("#howMuchLeft")
+
+var maxLength = area.prop("maxlength");
+console.log(maxLength);
+
+var currLength = area.val().length;
+
+area.keydown(function(){
+  currLength =area.val().length;
+  howMuchLeft.text((maxLength-currLength) + " Remaining characters");
+ });
